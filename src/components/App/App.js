8bf4e4deb7c837ad6e8daeb6742af2de83c2d8fb
Braedon <braedongough@gmail.com>
2019-02-02 15:35:23 +0100

    init commit

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div className="App">This is going to be a calculator</div>;
+  }
+}
+
+export default App;
